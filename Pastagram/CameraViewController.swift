f0706137620983144a63fab2185c5f101f457f9f
Bryan Jimenez <bryan@roguedevs.io>
2020-10-23 19:03:09 -0700

    Impleted query post function

--- Pastagram/CameraViewController.swift
+++ Pastagram/CameraViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -15,12 +16,28 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
 
         // Do any additional setup after loading the view.
     }
-    
+    @IBOutlet weak var commentField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var commentField: UIImageView!
     
     @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        post["caption"] = commentField.text!
+        post["author"] = PFUser.current()
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        post["image"] = file
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("Saved!")
+            }else{
+                print("Error")
+                }
+            }
+        
+        
     }
+    
     @IBAction func onCameraButton(_ sender: Any) {
         let picker = UIImagePickerController()
         picker.delegate = self
---
+++ Pastagram/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  Pastagram
+//
+//  Created by Bryan Jimenez on 10/23/20.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
